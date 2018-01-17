52a2bccfb7bdc31feab21cf14ef2b48e57a701db
wangyu <wangyu@192.168.0.109>
2018-01-17 16:23:43 +0800

    tesat

---
+++ pattern/src/main/java/com/java8/tutorial/concurrent/Lock1.java
@@ -0,0 +1,7 @@
+package com.java8.tutorial.concurrent;
+
+/**
+ * Created by wangyu on 06/01/2018
+ */
+public class Lock1 {
+}
---
+++ pattern/src/main/java/com/java8/tutorial/concurrent/Lock5.java
@@ -0,0 +1,7 @@
+package com.java8.tutorial.concurrent;
+
+/**
+ * Created by wangyu on 07/01/2018
+ */
+public class Lock5 {
+}
